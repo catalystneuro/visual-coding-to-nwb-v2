2ec362e78d1e5d5877a9fa58eadbaf4500177fae
Cody Baker <cbaker9@nd.edu>
2023-11-04 11:32:14 -0400

    add eye tracking

--- src/visual_coding_to_nwb_v2/visual_coding_ophys/visualcodingophyseyetrackinginterface.py
+++ src/visual_coding_to_nwb_v2/visual_coding_ophys/visualcodingophyseyetrackinginterface.py
@@ -21,10 +21,13 @@ class VisualCodingEyeTrackingInterface(BaseDataInterface):
 
             # x, y grid
             pupil_location_data = eye_tracking_source_data["pupil_location"]["data"]
+            number_of_columns = 2
+            max_frames_per_chunk = int(10e6 / (pupil_location_data.dtype.itemsize * number_of_columns))
+            data_chunks = (min(pupil_location_data.shape[0], max_frames_per_chunk), number_of_columns)
+
             pupil_location_timestamps = eye_tracking_source_data["pupil_location"]["timestamps"]
-            data_chunks = (min(pupil_location_data.shape[0], int(10e6 // (pupil_location_data.dtype.itemsize * 2))), 2)
             timestamp_chunks = min(
-                pupil_location_timestamps.shape[0], int(10e6 // pupil_location_timestamps.dtype.itemsize)
+                pupil_location_timestamps.shape[0], int(10e6 / pupil_location_timestamps.dtype.itemsize)
             )
 
             eye_tracking_spatial_series = list(
@@ -44,30 +47,37 @@ class VisualCodingEyeTrackingInterface(BaseDataInterface):
 
             # Angular space
             pupil_location_data_spherical = eye_tracking_source_data["pupil_location_spherical"]["data"]
-            pupil_location_timestamps_spherical = eye_tracking_source_data["pupil_location_spherical"]["timestamps"]
-            data_chunks_spherical = (
-                min(
-                    pupil_location_data_spherical.shape[0],
-                    int(10e6 // (pupil_location_data_spherical.dtype.itemsize * 2)),
-                ),
-                2,
+            number_of_columns_spherical = 2
+            max_frames_per_chunk_spherical = int(
+                10e6 / (pupil_location_data_spherical.dtype.itemsize * number_of_columns_spherical)
             )
+            data_chunks_spherical = (
+                min(pupil_location_data_spherical.shape[0], max_frames_per_chunk_spherical),
+                number_of_columns_spherical,
+            )
+
+            pupil_location_timestamps_spherical = eye_tracking_source_data["pupil_location_spherical"]["timestamps"]
             timestamp_chunks_spherical = min(
                 pupil_location_timestamps_spherical.shape[0],
-                int(10e6 // pupil_location_timestamps_spherical.dtype.itemsize),
+                int(10e6 / pupil_location_timestamps_spherical.dtype.itemsize),
             )
 
             eye_tracking_spatial_series_spherical = list(
                 SpatialSeries(
                     name="pupil_location_spherical",
                     description="Angle of pupil focus on the visual grid.",
-                    data=H5DataIO(data=pupil_location_data, compression=True, chunks=data_chunks),
-                    timestamps=H5DataIO(data=pupil_location_timestamps, compression=True, chunks=timestamp_chunks),
+                    data=H5DataIO(data=pupil_location_data_spherical, compression=True, chunks=data_chunks_spherical),
+                    timestamps=H5DataIO(
+                        data=pupil_location_timestamps_spherical, compression=True, chunks=timestamp_chunks_spherical
+                    ),
                     unit="degrees",
-                    reference_frame="(0,0) is the center of the monitor.",
+                    reference_frame=(
+                        "(0,0) is the center of the monitor; angle of incidence is calculated with respect to a "
+                        "right-facing vector."
+                    ),
                 )
             )
             eye_tracking_spherical = CompassDirection(spatial_series=eye_tracking_spatial_series_spherical)
 
             behavior_module = get_module(nwbfile=nwbfile, name="behavior", description="Processed behavioral data.")
-            behavior_module.add(eye_tracking)
+            behavior_module.add(eye_tracking_spherical)
