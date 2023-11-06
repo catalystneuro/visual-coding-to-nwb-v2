bf7fb8af158c7cccfa08c25614e2bbe0a392d4e2
Cody Baker <cbaker9@nd.edu>
2023-11-06 13:06:19 -0500

    reorganize filenames and add script

---
+++ src/visual_coding_to_nwb_v2/visual_coding_ophys/scripts/test_experiment_id_duplication.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import h5py
+
+base_path = Path("G:/visual-coding/ophys_experiment_data")
+v1_nwbfile_paths = base_path.rglob("*.nwb")
+
+all_duplication_testing = list()
+for v1_nwbfile_path in v1_nwbfile_paths:
+    with h5py.File(name=v1_nwbfile_path, mode="r") as v1_nwbfile:
+        ophys_experiment_id = v1_nwbfile["general"]["ophys_experiment_id"][()].decode("utf-8")
+    all_duplication_testing.append((ophys_experiment_id, v1_nwbfile_path.stem))
+
+for ophys_experiment_id, filename in all_duplication_testing:
+    assert ophys_experiment_id==filename
