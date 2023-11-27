200d6998bfd95db45e192a1d09d9cec7a76ecfc1
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-11-27 15:24:31 -0500

    Create examine_all_specimen_names_and_genotypes.py

---
+++ src/visual_coding_to_nwb_v2/visual_coding_ophys/scripts/examine_all_specimen_names_and_genotypes.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import h5py
+
+base_path = Path("G:/visual-coding/ophys_experiment_data")
+
+v1_nwbfiles = list(base_path.rglob("*.nwb"))
+
+all_specimen_names_and_genotypes = list()
+for v1_nwbfile in v1_nwbfiles:
+    file = h5py.File(name=v1_nwbfile, mode="r")
+
+    all_specimen_names_and_genotypes.append(
+        (
+            file["general"]["specimen_name"][()].decode("utf-8"),
+            file["general"]["subject"]["genotype"][()].decode("utf-8"),
+        )
+    )
+
+all_unique_specimen_names = list(set([x[0] for x in all_specimen_names_and_genotypes]))
+all_unique_genotypes = list(set([x[1] for x in all_specimen_names_and_genotypes]))
+all_unique_pairs = list(set(all_specimen_names_and_genotypes))
